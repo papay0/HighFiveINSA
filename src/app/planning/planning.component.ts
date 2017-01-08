058a3975f228ad57b75428582371e1069b34ad86
Arthur Papailhau <papailhau@gmail.com>
2017-01-08 18:51:24 +0100

    improve planning

--- src/app/planning/planning.component.ts
+++ src/app/planning/planning.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
 })
 export class PlanningComponent {
     selectedIndex = 0;
+    schedules = [
+        [
+            {description: 'Arrivée des participants', date: '06/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'},
+            {description: 'Arrivée des participants', date: '06/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'},
+            {description: 'Arrivée des participants', date: '06/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'},
+            {description: 'Arrivée des participants', date: '06/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'}
+        ],
+        [
+            {description: 'Blablabla', date: '07/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'},
+            {description: 'Blablabla', date: '07/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'},
+            {description: 'Blablabla', date: '07/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'},
+            {description: 'Blablabla', date: '07/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'}
+        ],
+        [
+            {description: 'A remplir', date: '08/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'},
+            {description: 'A remplir', date: '08/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'},
+            {description: 'A remplir', date: '08/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'},
+            {description: 'A remplir', date: '08/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'}
+        ]
+    
+    ];
     constructor() {}
 }
