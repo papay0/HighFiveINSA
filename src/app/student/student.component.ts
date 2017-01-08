11ede38f88af7fe2771fd87118bf7959a8d6ea2d
Arthur Papailhau <papailhau@gmail.com>
2017-01-08 17:18:36 +0100

    fix input button css

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -2,7 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-student',
-    templateUrl: './student.component.html'
+    templateUrl: './student.component.html',
+    styles: [`
+        md-card {
+            margin: 20px;
+        }
+        .file-over { border: dotted 3px red; }
+        .inputfile {
+            width: 0.1px;
+            height: 0.1px;
+            opacity: 0;
+            overflow: hidden;
+            position: absolute;
+            z-index: -1;
+        }
+    `]
 })
 export class StudentComponent {
     selectedValue = 0;
