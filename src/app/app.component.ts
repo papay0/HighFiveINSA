733e1fb9dac04d639e445efac281ce1411591a2e
Arthur Papailhau <papailhau@gmail.com>
2017-01-10 18:10:45 +0100

    fix flex height sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  window: Window;
+  urlGitHub: string = 'https://github.com/papay0/HighFiveINSA';
+
+  goGitHub(): void {
+    window.open(this.urlGitHub);
+  }
 }
