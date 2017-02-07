8eb552afeeb11cbc29988cfbedc355861feca718
Arthur Papailhau <papailhau@gmail.com>
2017-02-08 00:20:04 +0100

    improved design planning

--- src/app/planning/planning.component.ts
+++ src/app/planning/planning.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-planning',
-    templateUrl: './planning.component.html'
+    templateUrl: './planning.component.html',
+    styles: [`
+		md-card {
+		    margin: 8px;
+		}
+		md-card-content {
+		    padding-left: 10px;
+		}
+	`]
 })
 export class PlanningComponent {
     selectedIndex = 0;
@@ -25,7 +33,6 @@ export class PlanningComponent {
             {description: 'A remplir', date: '08/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'},
             {description: 'A remplir', date: '08/04/2017', location: 'Toulouse', timeTable: '08h00-09h30'}
         ]
-    
     ];
     constructor() {}
 }
